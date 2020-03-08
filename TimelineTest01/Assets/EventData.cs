721bf15842519a5c21c14e20e7d52da9deec5061
Seiki Okude <sokude@gmail.com>
2020-03-09 00:12:16 +0900

    Uploading

---
+++ TimelineTest01/Assets/EventData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EventData : ScriptableObject
+{
+    [SerializeField] private int intdata;
+    [SerializeField] private float floatdata;
+
+    public int Intdata { get => intdata; set => intdata = value; }
+    public float Floatdata { get => floatdata; set => floatdata = value; }
+}
---
+++ TimelineTest01/Assets/testobj.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testobj : MonoBehaviour
+{
+    public enum TestEnum
+    {
+        ONE, TWO, THREE
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Func01(int _arg)
+    {
+        Debug.Log($"int:{_arg}");
+    }
+    public void Func02(float _arg)
+    {
+        Debug.Log($"float:{_arg}");
+    }
+    public void Func03(string _arg)
+    {
+        Debug.Log($"string:{_arg}");
+    }
+    public void Func04(GameObject _arg)
+    {
+        Debug.Log($"GameObject:{_arg.name}");
+    }
+    public void Func05(TestEnum _arg)
+    {
+        Debug.Log($"Enum:{_arg}");
+    }
+    public void Func06(AnimationEvent _arg)
+    {
+        Debug.Log($"AnimationEvent float:{_arg}");
+    }
+
+    public void Func07(EventData _arg)
+    {
+        Debug.Log($"EventData Int:{_arg.Intdata}");
+        Debug.Log($"EventData float:{_arg.Floatdata}");
+    }
+
+}
